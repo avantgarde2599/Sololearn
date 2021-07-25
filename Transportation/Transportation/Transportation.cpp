784db141af45c9008406a8d6c9e812336e537ad4
avantgarde2599 <77590801+avantgarde2599@users.noreply.github.com>
2021-07-25 23:34:36 +0300

    Transportation

--- Transportation/Transportation/Transportation.cpp
+++ Transportation/Transportation/Transportation.cpp
@@ -5,9 +5,12 @@ int main() {
 	int bus_max = 50;
 	int passengers;
 	int last_bus;
+	int flights;
 	int free_places;
 	cout << "Enter the number of passengers: ";
 	cin >> passengers;
+	flights = passengers / bus_max;
+	cout << "The bus will make " << flights << " flights." << endl;
 	last_bus = passengers % bus_max;
 	cout << "Last bus will take " << last_bus << " passengers." << endl;
 	free_places = bus_max - last_bus;
